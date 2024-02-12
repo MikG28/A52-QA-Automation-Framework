8d11feb83d392379a0129f056b4a3ab3da901e71
MikG28 <mikayel.grigoryan@testpro.io>
2024-02-11 21:32:37 -0800

    Update ProfileTest.java

--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -27,3 +27,35 @@ public class ProfileTest extends BaseTest {
         Assert.assertEquals(newName, userNameLabel.getText());
     }
 }
+
++ import org.example.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+@@ -9,8 +10,9 @@ public class AddSongToPlayListTest extends BaseTest {
+    @Test
+    public void
+    addSongToPlayListTest() throws InterruptedException {
+        LoginPage page = new LoginPage(driver);
+        String songName = "Riqui-Riqui";
+        login("demo@class.com", "te$t$tudent");
+        page.login("demo@class.com", "te$t$tudent");
+        WebElement searchInput = driver.findElement(By.cssSelector("#searchForm>input"));
+        searchInput.sendKeys(songName);
+        searchInput.sendKeys(Keys.ENTER);
+
+        + import org.example.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+        @@ -9,8 +10,9 @@ public class AddSongToPlayListTest extends BaseTest {
+            @Test
+            public void
+            addSongToPlayListTest() throws InterruptedException {
+                LoginPage page = new LoginPage(driver);
+                String songName = "Riqui-Riqui";
+                login("demo@class.com", "te$t$tudent");
+                page.login("demo@class.com", "te$t$tudent");
+                WebElement searchInput = driver.findElement(By.cssSelector("#searchForm>input"));
+                searchInput.sendKeys(songName);
+                searchInput.sendKeys(Keys.ENTER);
